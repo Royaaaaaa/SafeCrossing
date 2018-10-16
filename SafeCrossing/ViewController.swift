70c58773cb74071902ad44bb466e67c3576ddfec
Royaaaaaa <43308173+Royaaaaaa@users.noreply.github.com>
2018-10-16 14:09:58 +1100

    Initial Commit

---
+++ SafeCrossing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafeCrossing
+//
+//  Created by 隋晓婷 on 2018/10/16.
+//  Copyright © 2018 Maya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
